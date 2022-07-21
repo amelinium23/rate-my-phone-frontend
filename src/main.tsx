480fc5ca4343141a93646d1403890c90f4b70c2e
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-07-21 08:17:15 +0200

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './App'
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
 )
