76f61246c4e2593376e079174b2e360a140dc46c
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-08-04 14:07:42 +0200

    new types

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -2,4 +2,14 @@ export interface Post {
   id: number
   title: string
   description: string
+  uid: string
+  votes: number
+  images: string[]
+  comments?: Comment[]
+}
+
+export interface Comment {
+  id: number
+  votes: number
+  comment?: string
 }
