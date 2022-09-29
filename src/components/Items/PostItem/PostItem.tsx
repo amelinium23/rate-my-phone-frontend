639404e0ff9c870605f4c8b285f3e1f478ac6f7e
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-09-29 15:49:44 +0200

    post page started

--- src/components/Items/PostItem/PostItem.tsx
+++ src/components/Items/PostItem/PostItem.tsx
@@ -1,61 +1,28 @@
-import axios from 'axios'
-import { FunctionComponent } from 'react'
-import { Button, Card, Container } from 'react-bootstrap'
-import { ArrowBigDown, ArrowBigTop } from 'tabler-icons-react'
+import './index.css'
 
-import { useStore } from '../../../context/Store'
+import { FunctionComponent } from 'react'
+import { Card, Container } from 'react-bootstrap'
+import { useNavigate } from 'react-router'
+
 import { Post } from '../../../types/Post'
 
 interface PostItemProps {
   post: Post
 }
 
-const upVotePost = async (post: Post) => {
-  const res = await axios.put('/forum/post', {
-    post_id: post.id,
-    ...post,
-    votes: post.votes + 1,
-  })
-  return res.data
-}
-
-const downVotePost = async (post: Post) => {
-  const res = await axios.put('/forum/post', {
-    ...post,
-    post_id: post.id,
-    votes: post.votes - 1,
-  })
-  return res.data
-}
-
 export const PostItem: FunctionComponent<PostItemProps> = ({ post }) => {
-  const { state } = useStore()
+  const navigate = useNavigate()
+
+  const handleNavigate = () => {
+    navigate(`/post/p/${post.id}`)
+  }
 
   return (
-    <Card className="my-1 p-2">
+    <Card className="my-1 p-2 post-item" onClick={handleNavigate}>
       <h5>{post.title}</h5>
       <p>{post.description}</p>
       <Container className="p-0 mb-0">
         <p>Votes {post.votes}</p>
-        {state.auth.currentUser && (
-          <>
-            <Button
-              style={{ marginRight: '5px' }}
-              onClick={() => upVotePost(post)}
-              size="sm"
-              variant="outline-light"
-            >
-              <ArrowBigTop size={15} stroke="black" />
-            </Button>
-            <Button
-              onClick={() => downVotePost(post)}
-              size="sm"
-              variant="outline-light"
-            >
-              <ArrowBigDown size={15} stroke="black" />
-            </Button>
-          </>
-        )}
       </Container>
     </Card>
   )
--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,5 +1,74 @@
-import { FunctionComponent } from 'react'
+import axios from 'axios'
+import { FunctionComponent, useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { useParams } from 'react-router'
+import { toast } from 'react-toastify'
+
+import { setIsLoading, useStore } from '../../context'
+import { Comment, Post } from '../../types/Post'
+
+// const upVotePost = async (post: Post) => {
+//   const res = await axios.put('/forum/post', {
+//     post_id: post.id,
+//     ...post,
+//     votes: post.votes + 1,
+//   })
+//   return res.data
+// }
+
+// const downVotePost = async (post: Post) => {
+//   const res = await axios.put('/forum/post', {
+//     ...post,
+//     post_id: post.id,
+//     votes: post.votes - 1,
+//   })
+//   return res.data
+// }
+
+const getPost = async (postId: number) => {
+  const res = await axios.get('/forum/find', { params: { id: postId } })
+  return res.data
+}
 
 export const PostPage: FunctionComponent = () => {
-  return <></>
+  const { dispatch } = useStore()
+  const { id } = useParams()
+  const postId = parseInt(id ?? '') || 0
+  const [post, setPost] = useState<Post | null>(null)
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        setIsLoading(dispatch, true)
+        const post = await getPost(postId)
+        setPost(post)
+      } catch (e) {
+        toast.error((e as Error).message)
+      } finally {
+        setIsLoading(dispatch, false)
+      }
+    }
+    fetchPost()
+  }, [])
+
+  return (
+    <Container className="mt-2">
+      <h5>{post?.title}</h5>
+      <section style={{ minHeight: '30vh' }}>
+        <p>{post?.description}</p>
+      </section>
+      <section>
+        <h5>Comments</h5>
+        {post?.comments ? (
+          <>
+            {post.comments.map((comment: Comment) => (
+              <>{JSON.stringify(comment)}</>
+            ))}
+          </>
+        ) : (
+          <p>No comments</p>
+        )}
+      </section>
+    </Container>
+  )
 }
